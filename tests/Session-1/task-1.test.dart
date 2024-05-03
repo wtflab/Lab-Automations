34d5ad97c4a2b2481f8b0854021c1f9ba43d2891
Pavel Dubrovskiy <paveldubrovskiyit@gmail.com>
2024-05-03 17:16:39 +0300

    Tests adjusted

---
+++ tests/Session-1/task-1.test.dart
@@ -0,0 +1,60 @@
+import 'package:test/test.dart';
+
+import 'task.dart';
+
+void main() {
+  group('Example tests', () {
+    test('Example test 1', () {
+      expect(reverseString('hello'), equals('olleh'));
+    });
+
+    test('Example test 2', () {
+      expect(reverseString('Dart'), equals('traD'));
+    });
+  });
+
+  group('Main tests', () {
+    test('Reversing a single character', () {
+      expect(reverseString('a'), equals('a'));
+    });
+
+    test('Reversing an empty string', () {
+      expect(reverseString(''), equals(''));
+    });
+
+    test('Reversing a string of digits', () {
+      expect(reverseString('123456789'), equals('987654321'));
+    });
+
+    test('Reversing a string with numbers', () {
+      expect(reverseString('12345'), equals('54321'));
+    });
+
+    test('Reversing a string with mixed uppercase and lowercase characters',
+        () {
+      expect(reverseString('AbCdEfG'), equals('GfEdCbA'));
+    });
+
+    test('Reversing a string with punctuation marks', () {
+      expect(reverseString('Hello, World!'), equals('!dlroW ,olleH'));
+    });
+
+    test('Reversing a string with whitespace', () {
+      expect(reverseString('Hello World'), equals('dlroW olleH'));
+    });
+
+    test('Reversing a long string', () {
+      expect(reverseString('Lorem ipsum dolor sit amet'),
+          equals('tema tis rolod muspi meroL'));
+    });
+
+    test('Reversing a string with special characters', () {
+      expect(reverseString('@!#HelloWorld'), equals('dlroWolleH#!@'));
+    });
+
+    test('Reversing a string with leading and trailing whitespaces', () {
+      expect(
+          reverseString('   Hello, World!   '), equals('   !dlroW ,olleH   '));
+    });
+  });
+}
---
+++ tests/Session-1/task-6.test.dart
@@ -0,0 +1,57 @@
+import 'package:test/test.dart';
+
+import 'task.dart';
+
+void main() {
+  group('Example tests', () {
+    test('Example test 1', () {
+      expect(firstUniqChar('What the Flutter'), 'W');
+    });
+
+    test('Example test 2', () {
+      expect(firstUniqChar('Flutter Flutter'), ' ');
+    });
+
+    test('Example test 3', () {
+      expect(firstUniqChar('Dart is great'), 'D');
+    });
+  });
+
+  group('Main tests', () {
+    test('Find first unique character at the beginning of the string', () {
+      expect(firstUniqChar('What the Flutter'), 'W');
+    });
+
+    test('Find first unique character in the middle of the string', () {
+      expect(firstUniqChar('Flutter Flutter'), ' ');
+    });
+
+    test('Find first unique character at the end of the string', () {
+      expect(firstUniqChar('Dart is great'), 'D');
+    });
+
+    test('Find first unique character in a string with all unique characters',
+        () {
+      expect(firstUniqChar('abcdefg'), 'a');
+    });
+
+    test('Find first unique character in an empty string', () {
+      expect(firstUniqChar(''), '');
+    });
+
+    test('Find first unique character in a string with all repeated characters',
+        () {
+      expect(firstUniqChar('aaaaa'), '');
+    });
+
+    test(
+        'Find first unique character in a string with multiple unique characters',
+        () {
+      expect(firstUniqChar('abc'), 'a');
+    });
+
+    test('Find first unique character in a string with special characters', () {
+      expect(firstUniqChar('!@#\$%^&*()'), '!');
+    });
+  });
+}
